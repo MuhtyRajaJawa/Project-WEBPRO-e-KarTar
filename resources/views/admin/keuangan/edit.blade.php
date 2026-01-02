09a805f349dc2d7c71bdc067cb0a9d05b851b6ee
shasha <meyrantishafyyaputri@gmail.com>
2026-01-02 11:26:01 +0700

    Update edit.blade.php

--- resources/views/admin/keuangan/edit.blade.php
+++ resources/views/admin/keuangan/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="card">
     <div class="card-body">
 
-        <h4><i class="fa fa-edit me-2"></i> Edit Transaksi</h4>
+        <h4><i class="fa fa-edit me-2"></i>Edit Transaksi</h4>
         <hr>
 
         <form action="{{ route('admin.keuangan.update', $data->id) }}" method="POST">
@@ -23,7 +23,7 @@
 
             <div class="mb-3">
                 <label>Tipe</label>
-                <select name="tipe" class="form-select"><!--GANTI INFO DISINI YAWW-->
+                <select name="tipe" class="form-select"><!--ganti info-->
                     <option value="pemasukan" {{ $data->tipe == 'pemasukan'?'selected':'' }}>Pemasukan</option>
                     <option value="pengeluaran" {{ $data->tipe == 'pengeluaran'?'selected':'' }}>Pengeluaran</option>
                 </select>
@@ -34,7 +34,7 @@
                 <input type="date" name="tanggal" value="{{ $data->tanggal }}" class="form-control"><!--ganti tanggal jd teks 'date, ganti 'text' disini-->
             </div>
 
-            <button class="btn btn-primary"><!--ini buat ganti teks/ iconnya-->
+            <button class="btn btn-primary"><!--ganti teks/iconnya-->
                 <i class="fa fa-save me-2"></i> Update
             </button>
 
